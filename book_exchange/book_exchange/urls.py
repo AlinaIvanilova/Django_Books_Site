fd0c01fe0e66a16f411d2b771b656dde0d316a98
AlinaIvanilova <alinatelf@gmail.com>
2025-05-22 16:28:01 +0300

    Code version 6 fix photo, add urls and I'M SO COOOOOL

--- book_exchange/book_exchange/urls.py
+++ book_exchange/book_exchange/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from books import views as book_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('books.urls')),
@@ -28,5 +31,6 @@ urlpatterns = [
     path('logout/', book_views.logout_view, name='logout'),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
